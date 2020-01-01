a82043213681a3a7cc48c412d45a42b9327517a9
Derek Chapman <derek4@illinois.edu>
2020-01-01 18:22:02 -0500

    Skip parsing metadata if there is no metadata.  Stop parsing at end of metadata section."
    /

--- convert_meta.py
+++ convert_meta.py
@@ -1,10 +1,11 @@
 # convert meta data from Notable into a folder structure for typora
 # move images and relink
 
-
+import os
 
 path_to_test_file = "./test_folder/Kegging.md"
-base_notes_path = '.'
+notes_path = '.'
+destination_path = './converted_notes/'
 
 def open_file(file_path):
 	document = []
@@ -14,15 +15,23 @@ def open_file(file_path):
 
 def parse_metadata(document):
 	metadata = {}
-	for line in document:
+
+	# check if there is even a header from notable, assumes the first char of the first line will be a dash
+	if document[0][0] != '-' : return metadata
+
+	# if there is metadata then get and organize it
+	for idx, line in enumerate(document):
 		split_line = line.split()
-		# print(split_line)
 		if len(split_line) < 1 : pass
 		elif split_line[0] == 'tags:' : metadata['tags'] = split_line[1].replace('[', '').replace(']', '')
 		elif split_line[0] == 'title:' : metadata['title'] = split_line[1]
 		elif split_line[0] == 'attachments:' : pass
+		elif idx > 1 and split_line[0] == '---' : return metadata
 
 	return metadata
+
+def set_title(document, metadata):
+	pass
 
 def create_folder():
 	# check if folder already exists
@@ -35,6 +44,6 @@ def remove_metadata():
 	pass
 
 document = open_file(path_to_test_file)
-# print(document)
 doc_metadata = parse_metadata(document)
+
 print(doc_metadata)
